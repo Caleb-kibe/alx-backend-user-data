1061882ab068af88397cc8962c2d327abc7364ef
caleb kibe <kiplagatcaleb2@gmail.com>
2024-09-03 16:55:06 +0300

    updated the method require_auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@
 
 from flask import request
 from typing import List, TypeVar
+import fnmatch
 
 
 class Auth:
@@ -11,6 +12,13 @@ class Auth:
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """checks if authentication is required for a given path"""
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+        for excluded_path in excluded_paths:
+            if fnmatch.fnmatch(path, excluded_path):
+                return False
         return True
 
     def authorization_header(self, request=None) -> str:
