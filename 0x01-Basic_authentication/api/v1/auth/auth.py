04f4e7843bb3a733c52c076e303f3c1db9d7bc86
caleb kibe <kiplagatcaleb2@gmail.com>
2024-09-03 16:23:12 +0300

    made changes

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,7 +13,7 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """checks if authentication is required for a given path"""
 
-        if path is None:
+        """if path is None:
             return True
 
         if excluded_paths is None or not excluded_paths:
@@ -21,14 +21,14 @@ class Auth:
 
         for excluded_path in excluded_paths:
             if fnmatch.fnmatch(path, excluded_path):
-                return False
+                return False"""
 
-        return True
+        return False
 
     def authorization_header(self, request=None) -> str:
         """retrieves the authorization header from the request"""
-        if request is not None:
-            return request.headers.get('Authorization', None)
+        #if request is not None:
+         #   return request.headers.get('Authorization', None)
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
