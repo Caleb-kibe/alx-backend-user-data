c5af2d9233b0c0bcb1e4aaab241b6f85fcbeafef
caleb kibe <kiplagatcaleb2@gmail.com>
2024-09-03 16:26:50 +0300

    changes

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -27,7 +27,7 @@ def stats() -> str:
 
 
 @app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
-def unauthorized() -> None:
+def unauthorized():
     """ GET api/v1/unauthorized
         raises a 401 error
     """
