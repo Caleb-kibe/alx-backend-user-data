b840e1879c47fee67fc9e3ddf5ef54b4ef840bf3
caleb kibe <kiplagatcaleb2@gmail.com>
2024-09-07 17:16:58 +0300

    updated the SessionAuth class

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,9 +3,21 @@
 """
 
 from .auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """ SessionAuth class that inherits from Auth
     """
-    pass
+    
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Creates a session ID for the user"""
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4)
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
