38d30fefa82d43969a76ff17808a6c3aa859da45
caleb kibe <kiplagatcaleb2@gmail.com>
2024-09-07 17:01:10 +0300

    created a class SessionAuth that inherits from Auth

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env pyhthon3
+""" SessionAuth model for API
+"""
+
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """ SessionAuth class that inherits from Auth
+    """
+    pass
